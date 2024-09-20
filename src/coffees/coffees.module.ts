a3b2a245e8f271add6b749bca4ffcd1cde0c796f
paragdangui <33660969+paragdangui@users.noreply.github.com>
2024-09-20 14:47:52 +0530

    mingration + joining tables + transactions

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
